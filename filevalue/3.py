84959e68337863c28524359ae78a876611bbf933
David <1350368559@qq.com>
2016-09-02 23:02:26 +0800

    test str tuple list dic

---
+++ filevalue/3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+num1 = input("Please inpput a numbder: ")
+num2 = input("Please inpput a numbder: ")
+
+print "%s + %s = %s" % (num1, num2, num1+num2)
+print "%s - %s = %s" % (num1, num2, num1-num2)
+print "%s * %s = %s" % (num1, num2, num1*num2)
+print "%s / %s = %s" % (num1, num2, num1/num2)
